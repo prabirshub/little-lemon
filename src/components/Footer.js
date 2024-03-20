a6d1ea9f4495e1d0c456496c26bc0f7e247e3ba8
prabirstack <160392884+prabirstack@users.noreply.github.com>
2024-03-20 12:09:11 +0530

    Footer html semantic  tag added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,51 @@
+import logo from '../assets/Logo.svg'
+
 export default function Footer() {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <div>
+        <div className='logo'>
+          <img src={logo} alt='Little Lemon' />
+        </div>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+      </div>
+      <div>
+        <h3>Important Links</h3>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <address>
+          <span>Address:</span>
+          <p>123 Town Street, Chicago</p>
+          <div>
+            <span>Phone:</span>
+            <a href='tel:+8731234567890'>+872 (123) 456‑7890</a>
+          </div>
+          <div>
+            <span>Email:</span>
+            <a href='mailto:hello@littlelemon.com'>hello@littlelemon.com</a>
+          </div>
+        </address>
+      </div>
+      <div>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+    </footer>
+  )
 }
